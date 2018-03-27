e690c127abe0f2ac5cd90ad87d75083f653b2989
inna golovataya <golovataya93@gmail.com>
2018-03-27 23:02:46 +0300

    new

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
  const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+ const keyboard = [];
 
- const keyboard = [['q', 'w', 'e', 'r', 't', 'y','u','i','o','p','[',']'],['a','s','d','f','g','h','j','k','l',';','\''],['z','x','c','v','b','n','m',',','.','/']];
- var firstWord = keyboard[1][5]+keyboard[0][2]+keyboard[1][8]+keyboard[1][8]+keyboard[0][8];
- var secondWord = keyboard[1][6]+keyboard[1][0]+keyboard[2][3]+keyboard[1][0]+keyboard[1][1]+keyboard[2][2]+keyboard[0][3]+keyboard[0][7]+keyboard[0][9]+keyboard[0][4];
- var thirthWord = keyboard[0][4]+keyboard[0][3]+keyboard[1][0]+keyboard[0][7]+keyboard[2][5]+keyboard[0][2]+keyboard[0][3];
- console.log(firstWord);
- console.log(secondWord);
- console.log(thirthWord);
+	for(i = 0; i < alphabet.length; i++){	
+		const string1 = alphabet.slice(0,12);
+		const string2 = alphabet.slice(12,23);
+		const string3 = alphabet.slice(23);
+		keyboard[0] = string1.split('');
+		keyboard[1] = string2.split('');
+		keyboard[2] = string3.split('');
+	};
+
+ console.log (keyboard[1][5]+keyboard[0][2]+keyboard[1][8]+keyboard[1][8]+keyboard[0][8]);
+ console.log(keyboard[1][6]+keyboard[1][0]+keyboard[2][3]+keyboard[1][0]+keyboard[1][1]+keyboard[2][2]+keyboard[0][3]+keyboard[0][7]+keyboard[0][9]+keyboard[0][4]);
+ console.log(keyboard[0][4]+keyboard[0][3]+keyboard[1][0]+keyboard[0][7]+keyboard[2][5]+keyboard[0][2]+keyboard[0][3]);
