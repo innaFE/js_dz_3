b331513050d43ab4cfb51af1999a7f19fd817c60
inna golovataya <golovataya93@gmail.com>
2018-04-14 22:54:03 +0300

    fresh

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
  const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
- const keyboard = [];
+ const arr = alphabet.split('');
 
-	
-		var string1 = alphabet.slice(0,12);
-		var string2 = alphabet.slice(12,23);
-		var string3 = alphabet.slice(23);
-		keyboard[0] = string1.split('');
-		keyboard[1] = string2.split('');
-		keyboard[2] = string3.split('');
+ const keyboard = [arr.slice(0, arr.indexOf('a')), arr.slice(arr.indexOf('a'), arr.indexOf('z')),arr.slice(arr.indexOf('z'))];	
+// const keyboard = [arr.slice(0, arr.indexOf("a")), arr.slice(arr.indexOf("a"), arr.indexOf("z")), arr.slice(arr.indexOf("z"))];
 
-		
-		const hello = keyboard[1][5]+keyboard[0][2]+keyboard[1][8]+keyboard[1][8]+keyboard[0][8];
-		const javascript = keyboard[1][6]+keyboard[1][0]+keyboard[2][3]+keyboard[1][0]+keyboard[1][1]+keyboard[2][2]+keyboard[0][3]+keyboard[0][7]+keyboard[0][9]+keyboard[0][4];
- 		const trainer = keyboard[0][4]+keyboard[0][3]+keyboard[1][0]+keyboard[0][7]+keyboard[2][5]+keyboard[0][2]+keyboard[0][3];
+ const hello = keyboard[1][5]+keyboard[0][2]+keyboard[1][8]+keyboard[1][8]+keyboard[0][8];
+ const javascript = keyboard[1][6]+keyboard[1][0]+keyboard[2][3]+keyboard[1][0]+keyboard[1][1]+keyboard[2][2]+keyboard[0][3]+keyboard[0][7]+keyboard[0][9]+keyboard[0][4];
+ const trainer = keyboard[0][4]+keyboard[0][3]+keyboard[1][0]+keyboard[0][7]+keyboard[2][5]+keyboard[0][2]+keyboard[0][3];
 
-		 console.log(`${hello}, ${javascript} ${trainer}`)
+ console.log(`${hello}, ${javascript} ${trainer}`)
